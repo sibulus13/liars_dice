20a56f527c504540d170f273c18d2d24d3d08c3d
sib <chengjie.michael.huang@gmail.com>
2023-10-24 20:25:49 -0700

    WIP refactored

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -12,45 +12,45 @@ import {
 // TODO: implement game logic
 // TODO: make this a 1 pg app so refresh doesn't just reset state
 export default function Game() {
-  const { name } = useSelector((state) => state.profile);
-  const { playerCount } = useSelector((state) => state.room);
+  let { playerNames } = useSelector((state) => state.room);
+  let { playerCount } = useSelector((state) => state.room);
+  let { playerIndex } = useSelector((state) => state.room);
+  let { dices } = useSelector((state) => state.room);
 
-  // randomly generate playerIndex as a number between 0 and playerCount - 1
-  const playerIndex = Math.floor(Math.random() * playerCount);
   // Generate random player names from a set list
-  const randomNames = [
-    "John",
-    "Jane",
-    "Joe",
-    "Jill",
-    "Jack",
-    "Jenny",
-    "Jim",
-    "Jen",
+
+  const actionButtons = [
+    { label: "Call BS", action: () => {} },
+    { label: "Bet", action: () => {} },
   ];
 
-  // randomly generate player names (array of length playerCount-1 filled with names) and add the user name to the end
-  const playerNames = Array(playerCount)
-    .fill('')
-    .map(() => randomNames[Math.floor(Math.random() * randomNames.length)])
-    .splice(playerIndex, name);
-
   // randomly generate dice values for all players (array of length playerCount filled with 5x 1-6)
-  let diceValues = [];
-  diceValues = Array(playerCount)
-    .fill(0)
-    .map(() =>
-      Array(5)
-        .fill(0)
-        .map(() => Math.floor(Math.random() * 6) + 1),
-    );
   // Keep track of last round of bets
   const [bets, setBets] = useState(Array(playerCount).fill([0, 0]));
+  // player inputs
+  const [dice, setDice] = useState(1);
+  const [bet, setBet] = useState(1);
   // Keep track of who's turn it is
   const [turn, setTurn] = useState(0);
-  // Keep track of who's turn it is to call BS
 
-  console.log(playerNames, playerIndex, diceValues);
+  const inputs = [
+    {
+      label: "Bet",
+      type: "range",
+      min: 1,
+      max: playerCount * 5,
+      value: bet,
+      onChange: setBet,
+    },
+    {
+      label: "Dice",
+      type: "range",
+      min: 1,
+      max: 6,
+      value: dice,
+      onChange: setDice,
+    },
+  ];
   return (
     <>
       <Head>
@@ -61,21 +61,60 @@ export default function Game() {
       <main className={mainStyle}>
         <div className={containerStyle}>
           {/* For Verification */}
-          <h1 className={headerStyle}>{name}</h1>
-          <h1 className={headerStyle}>{playerCount}</h1>
-        </div>
-        {/* Dice container */}
-        <div>
-          {/* Show dices based on diceValues */}
-          {playerNames}
-          {diceValues.map((player, index) => (
-            <div key={index}>
-              <p className={headerStyle}>{playerNames[index]}</p>
+          {/* <h1 className={headerStyle}>{name}</h1>
+          <h1 className={headerStyle}>{playerCount}</h1> */}
+          {/* Dice container */}
+          <div>
+            {dices.map((player, index) => (
+              <div key={index}>
+                <p className={headerStyle}>{playerNames[index]}</p>
+                <div className="my-2 flex gap-2">
+                  {player.map((dice, indexDice) => (
+                    <p key={indexDice} className={buttonStyle}>
+                      {index == playerIndex ? dice : "?"}
+                    </p>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+          {/* Controls container */}
+          <div className="p-4">
+            {/* inputs */}
+            <div className="py-2">
+              {inputs.map((input, index) => (
+                <div key={index}>
+                  <label className={headerStyle}>{input.label}</label>
+                  <div className="">
+                    <input
+                      type={input.type}
+                      id={input.label}
+                      name={input.label}
+                      min={input.min}
+                      max={input.max}
+                      value={input.value}
+                      onChange={(e) => input.onChange(e.target.value)}
+                    />
+                    {/* display the input number */}
+                    <p className={buttonStyle}>{input.value}</p>
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
+            {/* Action button container */}
+            <div className="flex justify-center gap-8 py-2">
+              {actionButtons.map((button, index) => (
+                <button
+                  key={index}
+                  className={buttonStyle}
+                  onClick={button.action}
+                >
+                  {button.label}
+                </button>
+              ))}
+            </div>
+          </div>
         </div>
-        {/* Controls container */}
-        <div></div>
       </main>
     </>
   );
